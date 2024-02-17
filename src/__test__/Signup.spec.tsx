925d5c632630c923ddd1797ff9029b2d10ed0b19
MinKyu Tae <alsrb8442@naver.com>
2024-02-17 22:41:43 +0900

    회원가입 실패 테스트 케이스 추가

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SignupPage from "../pages/SignupPage";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -29,9 +29,20 @@ describe("회원가입 테스트", () => {
     );
   });
 
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", () => {
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", async () => {
     // given - 회원가입 페이지가 그려짐
+
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+    const passwordInput = screen.findByLabelText("비밀번호");
+    const confirmPasswordInput = screen.findByLabelText("비밀번호 확인");
+
+    fireEvent.change(await passwordInput, { target: { value: "password" } });
+    fireEvent.change(await confirmPasswordInput, {
+      target: { value: "wrong-password" },
+    });
+
     // then - 에러메세지가 표시됨
+    const errorMessage = await screen.findByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -19,6 +19,9 @@ export default function SignupPage() {
     }
   }, [isSuccess]);
 
+  console.log(`[Log] password = ${password}`);
+  console.log(`[Log] confirmPassword = ${confirmPassword}`);
+
   return (
     <Wrapper>
       <div>
